e458d0315023bd376b7f2b04b3a35278df58151a
Denys Bilonozhko <deformix@gmail.com>
2021-06-12 13:40:33 +0300

    feat: implement error handling for empty task name

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import Button from './UI/Button';
+import ErrorModal from './UI/ErrorModal';
 
 const AddTask = ({ onAdd }) => {
   const [text, setText] = useState('');
   const [day, setDay] = useState('');
   const [reminder, setReminder] = useState(false);
+  const [error, setError] = useState();
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if (!text) {
-      alert('Please add a task');
+    if (!text || text.trim().length === 0) {
+      setError({
+        title: 'No task name',
+        message: 'Please enter a task name.',
+      });
       return;
     }
 
@@ -21,45 +26,60 @@ const AddTask = ({ onAdd }) => {
     setReminder(false);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <form
-      onSubmit={onSubmit}
-      className="border p-4 col-lg-8 col-sm-9 col-12 col-xl-7 col-xxl-6 mx-auto"
-    >
-      <div>
-        <label className="form-label">Task</label>
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Task name"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
         />
-      </div>
-      <div className="mb-3">
-        <label className="form-label">Day & Time</label>
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Add Day & Time"
-          value={day}
-          onChange={(e) => setDay(e.target.value)}
-        />
-      </div>
-      <div className="form-check mb-3">
-        <label className="form-check-label">Set Reminder</label>
-        <input
-          className="form-check-input"
-          type="checkbox"
-          checked={reminder}
-          value={reminder}
-          onChange={(e) => setReminder(e.currentTarget.checked)}
-        />
-      </div>
-      <div className="d-grid gap-2 col-12 mx-auto">
-        <Button className="btn-success btn btn-lg" type="submit">Save Task</Button>
-      </div>
-    </form>
+      )}
+      <form
+        onSubmit={onSubmit}
+        className="border p-4 col-lg-8 col-sm-9 col-12 col-xl-7 col-xxl-6 mx-auto"
+      >
+        <div>
+          <label className="form-label">Task</label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Task name"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Day & Time</label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Add Day & Time"
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+          />
+        </div>
+        <div className="form-check mb-3">
+          <label className="form-check-label">Set Reminder</label>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            checked={reminder}
+            value={reminder}
+            onChange={(e) => setReminder(e.currentTarget.checked)}
+          />
+        </div>
+        <div className="d-grid gap-2 col-12 mx-auto">
+          <Button className="btn-success btn btn-lg" type="submit">
+            Save Task
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
